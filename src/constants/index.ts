6f73fe6bb30dcc037985171e61b3df9f59513d70
raghvendrasingh-01 <raghvendrasingh1811@gmail.com>
2025-12-15 18:34:42 +0530

    feat: update project from Car Rent to Tax Calculator

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -203,9 +203,9 @@ const testimonials: TTestimonial[] = [
 
 const projects: TProject[] = [
   {
-    name: "Car Rent",
+    name: "Tax Calculator",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Web-based platform that allows users to calculate income tax, compare tax slabs, and estimate deductions, providing a convenient and efficient solution for tax planning needs.",
     tags: [
       {
         name: "react",
